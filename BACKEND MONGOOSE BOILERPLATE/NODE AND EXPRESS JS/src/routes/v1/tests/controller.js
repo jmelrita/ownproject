8103227e820ada01f4696cad0e100e88502e98aa
jmelrita <118337226+jmelrita@users.noreply.github.com>
2024-11-13 15:26:36 +0800

    Add files via upload

---
+++ BACKEND MONGOOSE BOILERPLATE/NODE AND EXPRESS JS/src/routes/v1/tests/controller.js
@@ -0,0 +1,130 @@
+import asyncHandler from "express-async-handler";
+import createError from "http-errors";
+import service from "./service.js";
+import { STATUSCODE } from "../../../constants/index.js";
+import {
+  upload,
+  responseHandler,
+  multipleImages,
+} from "../../../utils/index.js";
+
+const getAllTests = asyncHandler(async (req, res) => {
+  const data = await service.getAll();
+
+  responseHandler(
+    res,
+    data,
+    data?.length === STATUSCODE.ZERO
+      ? "No Tests found"
+      : "All Tests retrieved successfully",
+  );
+});
+
+const getAllTestsDeleted = asyncHandler(async (req, res) => {
+  const data = await service.getAllDeleted();
+
+  responseHandler(
+    res,
+    data,
+    data?.length === STATUSCODE.ZERO
+      ? "No Deleted Tests found"
+      : "All Deleted Tests retrieved successfully",
+  );
+});
+
+const getSingleTest = asyncHandler(async (req, res) => {
+  const data = await service.getById(req.params.id);
+
+  responseHandler(
+    res,
+    data,
+    !data ? "No Test found" : "Test retrieved successfully",
+  );
+});
+
+const createNewTest = [
+  upload.array("image"),
+  asyncHandler(async (req, res) => {
+    const uploadedImages = await multipleImages(req.files, []);
+
+    if (uploadedImages.length === STATUSCODE.ZERO)
+      throw createError(STATUSCODE.BAD_REQUEST, "Image is required");
+
+    const data = await service.add(
+      {
+        ...req.body,
+        image: uploadedImages,
+      },
+      req.session,
+    );
+
+    responseHandler(res, [data], "Test created successfully");
+  }),
+];
+
+const updateTest = [
+  upload.array("image"),
+  asyncHandler(async (req, res) => {
+    const oldData = await service.getById(req.params.id, session);
+
+    const uploadNewImages = await multipleImages(
+      req.files,
+      oldData?.image.map((image) => image.public_id) || [],
+    );
+
+    const data = await service.update(
+      req.params.id,
+      {
+        ...req.body,
+        image: uploadNewImages,
+      },
+      req.session,
+    );
+
+    responseHandler(res, [data], "Test updated successfully");
+  }),
+];
+
+const deleteTest = asyncHandler(async (req, res) => {
+  const data = await service.deleteById(req.params.id, req.session);
+
+  responseHandler(
+    res,
+    data?.deleted ? [] : [data],
+    data?.deleted ? "Test is already deleted" : "Test deleted successfully",
+  );
+});
+
+const restoreTest = asyncHandler(async (req, res) => {
+  const data = await service.restoreById(req.params.id, req.session);
+
+  responseHandler(
+    res,
+    !data?.deleted ? [] : [data],
+    !data?.deleted ? "Test is not deleted" : "Test restored successfully",
+  );
+});
+
+const forceDeleteTest = asyncHandler(async (req, res) => {
+  const data = await service.forceDelete(req.params.id, req.session);
+
+  const message = !data ? "No Test found" : "Test force deleted successfully";
+
+  await multipleImages(
+    [],
+    data?.image ? data.image.map((image) => image.public_id) : [],
+  );
+
+  responseHandler(res, [data], message);
+});
+
+export {
+  getAllTests,
+  getAllTestsDeleted,
+  getSingleTest,
+  createNewTest,
+  updateTest,
+  deleteTest,
+  restoreTest,
+  forceDeleteTest,
+};
---
+++ BACKEND MONGOOSE BOILERPLATE/NODE AND EXPRESS JS/src/routes/v1/users/route.js
@@ -0,0 +1,95 @@
+import { Router } from "express";
+import * as userController from "./controller.js";
+import { METHOD, PATH, ROLE } from "../../../constants/index.js";
+import { verifyJWT, authorizeRoles } from "../../../middlewares/index.js";
+
+const router = Router();
+
+const userRoutes = [
+  {
+    method: METHOD.GET,
+    roles: [ROLE.ADMIN],
+    middleware: [verifyJWT],
+    handler: userController.getAllUsers,
+  },
+  {
+    method: METHOD.GET,
+    path: PATH.DELETED,
+    roles: [ROLE.ADMIN],
+    middleware: [verifyJWT],
+    handler: userController.getAllUsersDeleted,
+  },
+  {
+    method: METHOD.POST,
+    path: PATH.LOGIN,
+    handler: userController.loginUser,
+  },
+  {
+    method: METHOD.POST,
+    path: PATH.LOGOUT,
+    handler: userController.logoutUser,
+  },
+  {
+    method: METHOD.GET,
+    path: PATH.ID,
+    roles: [ROLE.ADMIN],
+    middleware: [verifyJWT],
+    handler: userController.getSingleUser,
+  },
+  {
+    method: METHOD.POST,
+    handler: userController.createNewUser,
+  },
+  {
+    method: METHOD.PATCH,
+    path: PATH.EDIT,
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    middleware: [verifyJWT],
+    handler: userController.updateUser,
+  },
+  {
+    method: METHOD.DELETE,
+    path: PATH.DELETE,
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    middleware: [verifyJWT],
+    handler: userController.deleteUser,
+  },
+  {
+    method: METHOD.PUT,
+    path: PATH.RESTORE,
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    middleware: [verifyJWT],
+    handler: userController.restoreUser,
+  },
+  {
+    method: METHOD.DELETE,
+    path: PATH.FORCE_DELETE,
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    middleware: [verifyJWT],
+    handler: userController.forceDeleteUser,
+  },
+  {
+    method: METHOD.PATCH,
+    path: PATH.CHANGE_PASSWORD,
+    middleware: [verifyJWT],
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE, ROLE.CUSTOMER],
+    handler: userController.changeUserPassword,
+  },
+  {
+    method: METHOD.POST,
+    path: PATH.EMAIL_OTP,
+    handler: userController.sendUserEmailOTP,
+  },
+  {
+    method: METHOD.PATCH,
+    path: PATH.RESTORE_PASSWORD,
+    handler: userController.resetUserEmailPassword,
+  },
+];
+
+userRoutes.forEach((route) => {
+  const { method, path = "", roles = [], middleware = [], handler } = route;
+  router[method](path, middleware.concat(authorizeRoles(...roles)), handler);
+});
+
+export default router;
